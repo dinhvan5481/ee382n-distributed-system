081764cefd29a237ad115f4d7c9d2b67d4c97bcb
Van Quach <dinhvan5481@gmail.com>
2017-08-30 16:32:32 -0500

    working on store features

---
+++ hw1/src/ut/ee382n/ds/core/StoreItem.java
@@ -0,0 +1,56 @@
+package ut.ee382n.ds.core;
+
+public class StoreItem {
+    private String name;
+    private int qty;
+    public StoreItem(String name, int qty) {
+        this.name = name;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public synchronized StoreItem placeOrder(int qty) {
+        if(qty > this.qty) {
+            return null;
+        }
+        this.qty -= qty;
+        return createOrderItem(qty);
+    }
+
+    public synchronized void cancelOrder(int qty) {
+        this.qty += qty;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        StoreItem obj2 = (StoreItem)obj;
+        return obj2.equals(this.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", name, qty);
+    }
+
+    private StoreItem createOrderItem(int qty) {
+        return new StoreItem(name, qty);
+    }
+}
